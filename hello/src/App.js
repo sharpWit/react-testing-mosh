92d7a288102c06d5a11c436555ee2577a49ac8b4
saeedWebmaster <khosravi.webmaster@gmail.com>
2023-01-25 16:25:57 +0330

    modified:   hello/src/App.css 	modified:   hello/src/App.js 	new file:   hello/src/AppX.css 	new file:   hello/src/Components/Body.jsx 	new file:   hello/src/Components/Chat.jsx 	new file:   hello/src/Components/CleanChat.jsx 	new file:   hello/src/Components/Footer.jsx 	new file:   hello/src/Components/Heading.jsx

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,15 +1,18 @@
 // import "./Style.css";
+import "./App.css"
 import StickyNote from "./Components/StickyNote";
 import Clock from "./Components/Clock";
 import Click from "./Components/Click";
 import Main from "./Components/Main";
 import Conditional from "./Components/Conditional";
+import CleanChat from "./Components/CleanChat";
 import "./Main.css";
 function App() {
   return (
     <>
       <Main />
       <Conditional />
+      <CleanChat />
     </>
   );
 }
