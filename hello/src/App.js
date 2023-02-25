41734ee5b883e98883258378ad9f1651782431d0
saeedwebmaster <khosravi.webmaster@gmail.com>
2023-02-25 23:23:48 +0330

    modified:   hello/src/App.js 	new file:   hello/src/Components/auth/Main.jsx 	new file:   hello/src/Components/auth/UserInfo.jsx 	new file:   hello/src/Components/auth/auth-context.js

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import TextInput from "./Components/TextInput";
+import Main from "./Components/auth/Main";
+import AuthContext from "./Components/auth/auth-context";
 
 export default function App() {
+  const [user, setUser] = useState({ id: 1 });
   return (
     <>
-      <TextInput />
-      <TextInput />
-      <TextInput />
+      <AuthContext.Provider value={user}>
+        <Main />
+      </AuthContext.Provider>
     </>
   );
 }
---
+++ hello/src/Components/auth/UserInfo.jsx
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+import AuthContext from "./auth-context";
+
+export default function UserInfo() {
+  const user = useContext(AuthContext);
+  return <p>User ID: {user.id}</p>;
+}
