8670078293b115fa7e47e1522ee5bc00a3993ba8
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-04-12 18:40:56 +0330

    Testing rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,19 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("should render Hello when name prop is passed", () => {
+    render(<Greet name="Saeed" />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/Saeed/i);
+  });
+  it("should render login button when name prop is empty", () => {
+    render(<Greet />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
